92c87536b62fc8c0f45ff7c56dac5070f15e3e03
Martinez Machi <fdmz@gft.com>
2019-10-24 14:25:56 +0200

    added folder and files for data mocks

---
+++ src/app/core/mocks/menu.ts
@@ -0,0 +1,22 @@
+export const footerMenu = [
+  {
+    active: false,
+    label: 'Products and Services',
+    link: '',
+  },
+  {
+    active: false,
+    label: 'Contacts',
+    link: '',
+  },
+  {
+    active: false,
+    label: 'Security',
+    link: '',
+  },
+  {
+    active: false,
+    label: 'Terms and conditions of use/Disclaimer',
+    link: '',
+  },
+];
