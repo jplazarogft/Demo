6c62c850acae20a47c2ee5cf9f833bc9f85ba180
Martinez Machi <fdmz@gft.com>
2019-10-29 15:43:40 +0100

    added accordion component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
 import { HeadingComponent } from './components/heading/heading.component';
+import { AccordionComponent } from './components/accordion/accordion.component';
 
 @NgModule({
-  declarations: [HeadingComponent, NavMenuComponent, SearchInputComponent],
+  declarations: [HeadingComponent, NavMenuComponent, SearchInputComponent, AccordionComponent],
   imports: [CommonModule],
   exports: [HeadingComponent, NavMenuComponent, SearchInputComponent],
 })
