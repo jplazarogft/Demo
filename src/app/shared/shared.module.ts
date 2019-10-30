f00d029788e5bfbd54b66cdf8c41a75e701d7e95
Martinez Machi <fdmz@gft.com>
2019-10-30 18:01:02 +0100

    new icons added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,23 @@ import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
 import { HeadingComponent } from './components/heading/heading.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [HeadingComponent, NavMenuComponent, SearchInputComponent, AccordionComponent],
+  declarations: [
+    HeadingComponent,
+    NavMenuComponent,
+    SearchInputComponent,
+    AccordionComponent,
+    TableComponent,
+  ],
   imports: [CommonModule],
-  exports: [AccordionComponent, HeadingComponent, NavMenuComponent, SearchInputComponent],
+  exports: [
+    AccordionComponent,
+    HeadingComponent,
+    NavMenuComponent,
+    SearchInputComponent,
+    TableComponent,
+  ],
 })
 export class SharedModule {}
