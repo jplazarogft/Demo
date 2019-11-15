3e2bd6c3449b55f01d2fd78cb9d26928854aeef1
Martinez Machi <fdmz@gft.com>
2019-11-15 13:04:17 +0100

    added router to spec

--- src/app/detail/detail.component.spec.ts
+++ src/app/detail/detail.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AditionalContentComponent } from '../shared/components/aditional-content/aditional-content.component';
 import { DetailComponent } from './detail.component';
@@ -19,6 +20,7 @@ describe('DetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [
         AditionalContentComponent,
         DetailComponent,
