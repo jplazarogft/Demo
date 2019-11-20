ee1a3bf1a747a457ee99fbb15a7502a3b0db9167
Martinez Machi <fdmz@gft.com>
2019-11-20 17:15:23 +0100

    update modal component, updated buttons styles

--- src/app/shared/components/modal/modal.component.spec.ts
+++ src/app/shared/components/modal/modal.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalComponent } from './modal.component';
+import { ModalService } from './modal.service';
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
@@ -8,9 +9,9 @@ describe('ModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ModalComponent ]
-    })
-    .compileComponents();
+      declarations: [ModalComponent],
+      providers: [ModalService],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, TemplateRef } from '@angular/core';
+import { ModalService } from './modal.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
+  providers: [ModalService],
 })
 export class ModalComponent implements OnInit {
+  @Input() headerTemplate: TemplateRef<any>;
+  @Input() bodyTemplate: TemplateRef<any>;
+  @Input() footerTemplate: TemplateRef<any>;
   @Output() wrapperClick = new EventEmitter<boolean>();
 
-  constructor() {}
+  isVisible = new Observable<boolean>();
 
-  ngOnInit() {}
+  constructor(private modalService: ModalService) {}
+
+  ngOnInit() {
+    this.isVisible = this.modalService.visible$;
+  }
+
+  closeModal = () => this.modalService.toggleVisibility();
 
   sendEvent = () => this.wrapperClick.emit(true);
 }
