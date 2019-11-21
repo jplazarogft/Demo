2421165c3fc33889620053284297d6b9a820458f
Martinez Machi <fdmz@gft.com>
2019-11-21 11:28:54 +0100

    action menu  now accepts templaterefs for btn conf

--- src/app/shared/components/action-menu/action-menu.component.ts
+++ src/app/shared/components/action-menu/action-menu.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-action-menu',
   templateUrl: './action-menu.component.html',
-  styleUrls: ['./action-menu.component.scss']
+  styleUrls: ['./action-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ActionMenuComponent implements OnInit {
+  @Input() menuBody: TemplateRef<any>;
+  @Input() menuHidden: TemplateRef<any>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
