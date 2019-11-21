e9d1ef04fabcd888dee743b94fe79bff3de016f9
Martinez Machi <fdmz@gft.com>
2019-11-21 11:48:44 +0100

    added show modal function

--- src/app/home/home-header/home-header.component.ts
+++ src/app/home/home-header/home-header.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Icon } from '@sharedModels/icon';
+import { ModalService } from '../../shared/components/modal/modal.service';
 
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.scss'],
+  providers: [ModalService],
 })
 export class HomeHeaderComponent implements OnInit {
   @Input() icons: Icon[] = [];
   @Input() title = '';
 
-  constructor() {}
+  constructor(private modalService: ModalService) {}
 
   ngOnInit() {}
+
+  showCreateAppModal = () => this.modalService.toggleVisibility();
 }
