4f8d2f989f0d42de77fd14958c7507c75ad07110
Martinez Machi <fdmz@gft.com>
2019-11-22 10:50:24 +0100

    sorted dependencies

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 // Components
 import { FooterComponent } from './components/footer/footer.component';
