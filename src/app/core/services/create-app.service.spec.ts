0a9ef7e2096145d38312257632ea75d4f2e0ab77
Martinez Machi <fdmz@gft.com>
2019-11-22 10:48:07 +0100

    added missing dependency to test

--- src/app/core/services/create-app.service.spec.ts
+++ src/app/core/services/create-app.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CreateAppService } from './create-app.service';
 
 describe('CreateAppService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    }),
+  );
 
   it('should be created', () => {
     const service: CreateAppService = TestBed.get(CreateAppService);
